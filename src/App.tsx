0caeca3084885db3a832bbf5d5446cd270fc1e32
Rywin Patcharaput <rywinp@umich.edu>
2025-07-13 01:53:03 -0400

    Initialized Repo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Stack } from '@mui/material'
+
+function App() {
+  return (
+    <Stack direction={"column"} spacing={1}>
+
+    </Stack>
+  )
+}
+
+export default App
