ccdd4b522b64cf65ab8a7fff323645c15f584eda
Rywin Patcharaput <rywinp@umich.edu>
2025-07-14 01:14:37 -0400

    Window Size friendly adjustments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,25 @@ import Lions from "../assets/Lions.jpg"
 
 export default function Header() {
     return (
-        <Box sx={{position: "relative", width: "100%", height: { xs: 200, xl: 500 }, backgroundImage: `url(${Lions})`, backgroundSize: "cover", backgroundPosition: "top", borderRadius: 2 }}>
-            <Typography variant="h1" sx={{position: "absolute", bottom: 0, left: 0, padding: 2}}>Rywin Patcharaput</Typography>
+        <Box sx={{
+            position: "relative",
+            width: "100%",
+            height: { xs: 200, sm: 300, md: 350, lg: 450, xl: 575 },
+            backgroundImage: `url(${Lions})`,
+            backgroundSize: "cover",
+            backgroundPosition: "top",
+            borderRadius: 2
+        }}>
+            <Typography variant="h1" sx={{
+                position: "absolute", bottom: 0, left: 0, padding: 2, fontSize: {
+                    xs: "2rem",
+                    sm: "3rem",
+                    md: "4rem",
+                    lg: "6rem",
+                    xl: "7rem",
+                },
+                fontWeight: 600,
+            }}>Rywin Patcharaput</Typography>
         </Box>
     );
 }
